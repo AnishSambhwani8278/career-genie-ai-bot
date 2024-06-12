2507507a4615d6f91dc9863b0d9ad178e7819c5b
AnishSambhwani8278 <166947483+AnishSambhwani8278@users.noreply.github.com>
2024-06-12 17:23:52 +0530

    app.py

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -10,7 +10,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Configure Google AI API with the API key directly
-genai.configure(api_key="AIzaSyBP8bNL0bHyurTuaCZ7kHyfX88Ie5byRAk")
+genai.configure(api_key="INPUT YOUR API")
 
 # Create the model
 generation_config = {
